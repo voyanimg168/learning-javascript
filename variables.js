7c529e46cd21562e501f69496e99740f1829144e
Minru Gan <minrugan@gmail.com>
2025-05-30 17:20:03 -0600

    add modif to variables.js

--- variables.js
+++ variables.js
@@ -15,3 +15,36 @@ console.log(a)
 // BOOLEANS
 let b = true
 console.log(b)
+
+// REASSIGNING VARIABLES
+let c = 3
+console.log(c)
+c = 4  
+console.log(c)
+
+// CONSTANTS
+const d = 6 
+console.log(d)
+
+// OPERATIONS ON NUMBERS
+let e = 3
+let f = 4
+let g = e + f
+console.log(g)
+
+let h = e - f
+console.log(h)
+
+let i = e * f
+console.log(i)
+
+let j = e / f
+console.log(j)
+
+let k = f % e // Modulus operator
+console.log(k)
+
+e = e + 1 // Incrementing e
+console.log(e)
+f = f - 1 // Decrementing f
+console.log(f)
