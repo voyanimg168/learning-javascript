711378317ee237b466ceb22d93b44880a2bfa1bb
Minru Gan <minrugan@gmail.com>
2025-05-30 16:40:27 -0600

    add variables.js

---
+++ variables.js
@@ -0,0 +1,17 @@
+// NUMBERS 
+
+let x = 5
+console.log(x)
+
+let y = 5.5
+console.log(y)
+
+// STRINGS
+let z = "hello"
+console.log(z)
+let a = "A string can be a whole sentence." 
+console.log(a)
+
+// BOOLEANS
+let b = true
+console.log(b)
