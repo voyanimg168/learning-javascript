5d0a35bcf2078584717e87f69cb1d3a06d8b5fa6
Minru Gan <minrugan@gmail.com>
2025-05-31 11:54:04 -0600

    functions.js

---
+++ functions.js
@@ -0,0 +1,16 @@
+function myFirstFunction() {
+    console.log("This is my first function!");
+}
+myFirstFunction(); // This is my first function!
+
+function mySecondFunction(name) {
+    console.log("Hello, " + name + "!");
+}
+mySecondFunction("Kristin");
+mySecondFunction("Fred");
+
+function addTwoNumbers(x, y) {
+    return x + y;
+}
+console.log(addTwoNumbers(5, 10)); // 15
+console.log(addTwoNumbers(3, 7)); // 10 git statuss
