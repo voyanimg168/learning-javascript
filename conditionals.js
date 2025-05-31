14a4638d673ef29c107f6f0fdda98d8aaa947872
Minru Gan <minrugan@gmail.com>
2025-05-30 19:03:45 -0600

    conditionals.js alarm clock

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,5 @@
+const prompt = require('prompt-sync')();
+
 let x = 4
 let y = 5
 if (y > x) {
@@ -8,4 +10,13 @@ if (x > y) {
     console.log("x is greater than y");
 } else {
     console.log("x is not greater than y");
+}
+// A program to determine whether it's time to get up
+let hour = prompt("What time is it? (0-23): ");
+if (hour < 4) {
+    console.log("It's too early to get up. Keep sleeping!");
+} else if(hour > 4 && hour < 7) {
+    console.log("It's time to get up, but you can snooze for a bit longer.");
+} else {
+    console.log("It's time to get up and start your day!");
 }
