1100ec235327172bca464b12c7976e7924ff7b54
Minru Gan <minrugan@gmail.com>
2025-05-30 18:28:12 -0600

    comparisons.js

---
+++ comparisons.js
@@ -0,0 +1,13 @@
+console.log("Does 5 equal 4? " + (5 == 4))
+console.log("Does 5 equal 5? " + (5 == 5))  
+console.log("Does 5 not equal 4? " + (5 != 4))
+
+console.log("Is 5 greater than 4? " + (5 > 4))
+console.log("Is 5 less than 4? " + (5 < 4))
+console.log("Is 5 greater than or equal to 4? " + (5 >= 4)) 
+console.log("Is 5 less than or equal to 5? " + (5 <= 5))
+
+let x = 3
+let y = 2
+console.log("Is x greater than y? " + (x > y))
+console.log("Is x equal to y? " + (x == y))
