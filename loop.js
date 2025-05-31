e5d8228b0fca0d237a64cb8fdfb6962e53181eea
Minru Gan <minrugan@gmail.com>
2025-05-31 10:17:05 -0600

    loop.js

---
+++ loop.js
@@ -0,0 +1,18 @@
+// For Loop (3 parts)
+
+// Part 1: Creates a counter variable
+
+// Part 2: Sets condition in which loop will execute
+
+// Part 3: Increments the counter variable
+
+for(x = 1; x < 5; x++) {
+    console.log("x is " + x)
+}
+
+let y = "foo"
+for(x = 1; x < 5; x++) {
+    y = y + "bar"
+    console.log(y)
+}
+
