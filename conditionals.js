ccb062f463bff6a383a6ba7eef52d100cef3a136
Minru Gan <minrugan@gmail.com>
2025-05-30 18:36:10 -0600

    conditionals.js

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+let x = 4
+let y = 5
+if (y > x) {
+    console.log("y is greater than x");
+}
+
+if (x > y) {
+    console.log("x is greater than y");
+} else {
+    console.log("x is not greater than y");
+}
